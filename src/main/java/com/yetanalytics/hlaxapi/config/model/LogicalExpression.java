9c456fe0b6a46c236da6ca0fa6bf5a7d48cae83e
Cliff Casey <cliff@yetanalytics.com>
2026-06-17 11:13:26 -0400

    PR cleanup

--- src/main/java/com/yetanalytics/hlaxapi/config/model/LogicalExpression.java
+++ src/main/java/com/yetanalytics/hlaxapi/config/model/LogicalExpression.java
@@ -3,10 +3,10 @@ package com.yetanalytics.hlaxapi.config.model;
 import java.util.List;
 
 public class LogicalExpression implements Expression {
-    public final com.yetanalytics.hlaxapi.config.model.LogicalOperator operator;
+    public final LogicalOperator operator;
     public final List<Expression> operands;
 
-    public LogicalExpression(com.yetanalytics.hlaxapi.config.model.LogicalOperator operator, List<Expression> operands) {
+    public LogicalExpression(LogicalOperator operator, List<Expression> operands) {
         this.operator = operator;
         this.operands = operands;
     }
